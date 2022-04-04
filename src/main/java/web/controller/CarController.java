a2b0438760ec75df3df9f9f87e17ea6fce265679
RadelNigma <radelnigma@yandex.ru>
2022-04-04 07:45:48 +0300

    com1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,43 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.models.Car;
+import web.service.ServiceClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+//    final
+    ServiceClass serviceClass;
+
+    public CarController(ServiceClass serviceClass) {
+        this.serviceClass = serviceClass;
+    }
+
+    @GetMapping(value = "/car")
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model){
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car(1 ,"Audi",100));
+        cars.add(new Car(2 ,"Bentley",200));
+        cars.add(new Car(3 ,"Citroen",300));
+        cars.add(new Car(4 ,"Dodge",400));
+        cars.add(new Car(5 ,"Tesla",500));
+
+        if(count==0){count=cars.size();}
+
+        List<Car> carList = serviceClass.getCars(cars,count);
+
+        model.addAttribute("carList", carList);
+        return "cars";
+    }
+
+
+
+}
---
+++ src/main/java/web/service/ServiceClass.java
@@ -0,0 +1,14 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.models.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ServiceClass {
+    public List<Car> getCars (List<Car> cars , int num){
+        return cars.stream().limit(num).collect(Collectors.toList());
+    }
+}
