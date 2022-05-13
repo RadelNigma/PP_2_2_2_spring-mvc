0a120108118737c385aa300ccd17e64a00dff69d
RadelNigma <radelnigma@yandex.ru>
2022-05-14 01:52:28 +0300

    corrected version

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,22 +5,22 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-import web.service.ServiceClass;
+import web.service.CarServiceImpl;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    ServiceClass serviceClass;
+    CarServiceImpl serviceClass;
 
-    public CarController(ServiceClass serviceClass) {
+    public CarController(CarServiceImpl serviceClass) {
         this.serviceClass = serviceClass;
     }
 
     @GetMapping(value = "/car")
-    public String getCars(@RequestParam(value = "count", required = false, defaultValue = "0") int count, ModelMap model) {
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
 
-        List<Car> carList = serviceClass.getCars(serviceClass.getCars(),count);
+        List<Car> carList = serviceClass.getCars(count);
 
         model.addAttribute("carList", carList);
         return "cars";
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -8,21 +8,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ServiceClass {
+public class CarServiceImpl implements CarService {
 
-    public List<Car> getCars() {
-        List<Car> cars = new ArrayList<>();
+    private List<Car> cars = new ArrayList<>();
 
+    {
         cars.add(new Car(1, "Audi", 100));
         cars.add(new Car(2, "Bentley", 200));
         cars.add(new Car(3, "Citroen", 300));
         cars.add(new Car(4, "Dodge", 400));
         cars.add(new Car(5, "Tesla", 500));
-
-        return cars;
     }
 
-    public List<Car> getCars(List<Car> cars, int num) {
-        return num == 0 ? cars : cars.stream().limit(num).collect(Collectors.toList());
+    @Override
+    public List<Car> getCars(Integer count) {
+        return count == null ? cars : cars.stream().limit(count).collect(Collectors.toList());
     }
 }
