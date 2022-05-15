dbcb42e2c7c4f504544cf15db6381cd16a23944b
RadelNigma <radelnigma@yandex.ru>
2022-05-16 01:03:27 +0300

    Added CarService interface

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,15 +5,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    CarServiceImpl serviceClass;
+    CarService serviceClass;
 
-    public CarController(CarServiceImpl serviceClass) {
+    public CarController(CarService serviceClass) {
         this.serviceClass = serviceClass;
     }
 
